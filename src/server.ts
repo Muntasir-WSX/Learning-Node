7ecb461429705f2e33a567a1da4407f22086ca7c
Muntasir <alimuntasir2001@gmail.com>
2026-05-11 23:28:19 +0600

    Route handling done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,29 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req : IncomingMessage , res) => {
-    console.log(req);
+const server: Server = createServer((req : IncomingMessage , res:ServerResponse) => {
+    // console.log(req.url); // '/', '/user', '/products'
+    // console.log(req.method); // 'GET', 'POST', 'PUT', 'DELETE'
+
+    const url = req.url;
+    const method = req.method;
+    if (url === "/" && method === "GET") {
+        // console.log("This is root route");
+        res.writeHead(200,{"content-type" : "application/json"})
+        res.end(JSON.stringify({ message: "Welcome to the root route" }));
+    }
+
+    else if (url?.startsWith("products"))
+    {
+        res.writeHead(200,{"content-type" : "application/json"})
+        res.end(JSON.stringify({ message: "Welcome to the products route" }));
+    }
+
+    else  {
+
+        res.writeHead(404, {"content-type" : "application/json"})
+        res.end(JSON.stringify({ error: "Page not found" }));
+    }
+
 });
 
 server.listen(3000, () => {
