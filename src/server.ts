97d38bf4e29adffd4a949b6784c7df2b7a64eb4b
Muntasir <alimuntasir2001@gmail.com>
2026-05-16 00:32:47 +0600

    config handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 import { routeHandler } from "./routes/routes";
+import config from "./config";
 
 const server: Server = createServer((req : IncomingMessage , res:ServerResponse) => {
   routeHandler(req, res);
 });
 
-server.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+server.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
 });
